a1d808cfe17d0f129c0df6508c91a645222a8e4c
henryantwi <henryantwi191@gmail.com>
2024-12-23 09:08:22 +0000

    "Remove redundant comments and clean up settings.py"
    Tidied up `settings.py` by eliminating unused comments and unnecessary lines related to quick-start guidance and security warnings. This makes the file cleaner and easier to maintain without altering functional configurations.

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,23 +1,16 @@
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-17=jwc_h%+8p37qvrg^$-h8dkg8tg2s!n=*t_qp(j%e&at)_(e"
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
 
-# Application definition
 
 INSTALLED_APPS = [
     # 'jazzmin',
